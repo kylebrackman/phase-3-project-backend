672a4f39b97cce74673ee5e135879b1855c4c0ce
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-01-18 18:22:47 -0700

    added delete request to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
   get "/items" do
     items = Item.all
     items.to_json(include: [:reviews])
+  end
+
+  delete "/items/:id" do
+    item = Item.find params[:id]
+    item.destroy
+    item.to_json
   end
 end
